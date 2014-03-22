676af394b250d00fce82c30ea344644c5dbdf745
Ilya Beda <ir4y.ix@gmail.com>
2014-03-22 19:22:14 +0800

    Pep8

--- server/timetracker_flask/tasks.py
+++ server/timetracker_flask/tasks.py
@@ -4,16 +4,19 @@ from app import User, Project, Task
 
 
 def make_celery(app):
-    celery = Celery(app.import_name, backend='amqp', broker=app.config['CELERY_BROKER_URL'])
+    celery = Celery(app.import_name,
+                    backend='amqp',
+                    broker=app.config['CELERY_BROKER_URL'])
     celery.conf.update(app.config)
     celery.conf.update(
         CELERY_TASK_SERIALIZER='json',
         CELERY_ACCEPT_CONTENT=['json'],  # Ignore other content
-        CELERY_RESULT_SERIALIZER='mongo_json',
-        )
+        CELERY_RESULT_SERIALIZER='mongo_json', )
     TaskBase = celery.Task
+
     class ContextTask(TaskBase):
         abstract = True
+
         def __call__(self, *args, **kwargs):
             with app.app_context():
                 return TaskBase.__call__(self, *args, **kwargs)
