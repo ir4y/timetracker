930f38bc2e686f79d557cf07d702efb9269e1e4f
Лаврёнов Антон <lavrton@gmail.com>
2014-03-22 15:16:33 +0800

    css concat

--- client/gruntFile.js
+++ client/gruntFile.js
@@ -3,17 +3,24 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-      concat: {
-        options: {
-          separator: ';',
-        },
+    concat: {
+      options: {
+        separator: ';',
+      },
       dist: {
-        src: ['vendor/angular/angular.js',
+        src: ['vendor/jquery/dist/jquery.js',
+              'vendor/bootstrap/dist/js/bootstrap.js',
+              'vendor/angular/angular.js',
               'vendor/angular-rpc/src/angular-rpc.js',
               'vendor/angular-ui-router/release/angular-ui-router.js',
               'src/**/*.js'],
         dest: 'application.js',
       },
+      css : {
+        src : ['vendor/bootstrap/dist/css/bootstrap.css'],
+        dest : 'main.css'
+
+      }
     },
     watch: {
       dev: {
